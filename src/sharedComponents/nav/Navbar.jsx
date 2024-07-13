6dd3cb890e0fcc80d02d604d0cfc7c02415af1a7
Sourov <sourovdas730@gmail.com>
2024-07-13 14:40:34 +0600

    upd : do responsive nav

--- src/sharedComponents/nav/Navbar.jsx
+++ src/sharedComponents/nav/Navbar.jsx
@@ -4,9 +4,11 @@ import { LiaShoppingBagSolid } from 'react-icons/lia';
 import logo from '../../assets/4oclock.webp'
 import { IoSearchOutline } from 'react-icons/io5';
 import { TfiClose } from 'react-icons/tfi';
+import { VscChromeClose, VscClose, VscMenu } from 'react-icons/vsc';
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
+    const [menu, setMenu] = useState(false);
     const searchRef = useRef(null);
     console.log(searchRef);
     console.log(open);
@@ -28,21 +30,25 @@ const Navbar = () => {
     }, [])
     return (
         <>
-            <nav className='flex justify-between items-center py-6 font-montserrat text-[#01204E] max-w-7xl mx-auto'>
-                <img className='w-48' src={logo} alt="" />
-                <div className='flex justify-center items-center gap-6'>
-                    <a href="">Home</a>
-                    <a href="">All Watches</a>
+            <nav className='flex justify-between items-center h-16 border-b lg:border-none font-montserrat text-[#01204E] max-w-6xl mx-auto px-4 lg:px-0 relative'>
+                <button onClick={() => setMenu(!menu)} className='lg:hidden text-[#01204E] text-xl lg:text-2xl'>{!menu ? <VscMenu /> : <VscChromeClose />}</button>
+                <img className='w-28 lg:w-48' src={logo} alt="" />
+                {/* Route Section */}
+                <div className={`flex justify-center items-center lg:gap-6 absolute lg:static text-center ${menu ? 'top-16 left-0 flex-col lg:flex-row bg-white lg:bg-white w-full' : 'hidden'}`}>
+                    <a className='hover:bg-gray-100 lg:hover:bg-white w-full lg:w-auto py-3 border-b lg:border-none' href="">Home</a>
+                    <a className='hover:bg-gray-100 lg:hover:bg-white w-full lg:w-auto py-3 border-b lg:border-none' href="">All Watches</a>
                 </div>
-                <div className='flex justify-center items-center gap-6 text-2xl'>
+                {/* user and cart section */}
+                <div className='flex justify-center items-center gap-4 lg:gap-6 text-xl lg:text-2xl'>
                     <button><AiOutlineUser /></button>
                     <button className='hover:scale-125 duration-200' onClick={() => setOpen(true)}><IoSearchOutline /></button>
                     <button><LiaShoppingBagSolid /></button>
                 </div>
 
             </nav>
+            {/* search section */}
             <div ref={searchRef} className={`bg-white h-24 w-full absolute top-0 mx-auto transition-transform flex justify-center items-center gap-6 border-b ${open ? 'translate-y-0 duration-300' : '-translate-y-full'}`}>
-                <div className='w-1/3 relative'>
+                <div className='w-2/3 lg:w-1/3 relative'>
                     <input className='py-3 px-3 border border-gray-400 w-full focus:outline-none focus:border-2 hover:border-2 focus:border-black' type="text" placeholder='Search' />
                     {/* <span className='absolute left-4 top-3'>Search</span> */}
                 </div>
